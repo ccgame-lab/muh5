e428a624656871706c2adb2652f15f408de21cc3
MUH5 Admin <admin@muh5.com>
2026-05-15 18:23:08 +0700

    Add safe config template

---
+++ config.example.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'app_name' => 'MUH5',
+    'debug' => false,
+
+    'greenjade_id' => [
+        'base_url' => 'https://server.greenjade.net',
+        'client_id' => '',
+        'client_secret' => '',
+        'redirect_uri' => 'https://muh5.ccgame.org/callback.php',
+    ],
+
+    'game' => [
+        'slug' => 'muh5',
+        'server_id' => 1,
+    ],
+];
